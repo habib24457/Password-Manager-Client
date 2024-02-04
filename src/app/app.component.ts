8d5436973fb322724bce2dc67cfc527c64bffb68
habib24457 <habiburehman390@gmail.com>
2024-02-04 14:15:37 +0100

    Added table for showing api response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataDisplayComponent } from './data-display/data-display.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DataDisplayComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'password-manager-client';
+  title = 'Password Manager'; //title is a global variable
+
+  //method
+  getSubTitle() {
+    const subTitle = '**Encrypt and Decrypt your password**';
+    return subTitle;
+  }
+
+  //get user password method
 }
---
+++ src/app/data-display/data-display.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+interface IPasswordItem {
+  id: number;
+  userName: string;
+  app: string;
+  category: string;
+  userPassword: string;
+}
+
+@Component({
+  selector: 'app-data-display',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
+  templateUrl: './data-display.component.html',
+  styleUrl: './data-display.component.css',
+})
+export class DataDisplayComponent implements OnInit {
+  httpClient = inject(HttpClient);
+  passwordData: IPasswordItem[] = [];
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  //get all the user passwords
+  async fetchData() {
+    await this.httpClient
+      .get('http://localhost:5014/api/Passwords')
+      .subscribe((data: any) => {
+        console.log('Raw data with password encryption', data);
+        let decryptedPasswordItems = this.decryptPassword(data);
+        console.log('After password decrypting', decryptedPasswordItems);
+        this.passwordData = decryptedPasswordItems;
+      });
+  }
+
+  decryptPassword(encryptedPasswordItems: IPasswordItem[]) {
+    console.log(encryptedPasswordItems);
+    let updatedItems: IPasswordItem[] = [...encryptedPasswordItems];
+    for (let i = 0; i < encryptedPasswordItems.length; i++) {
+      let base64String = encryptedPasswordItems[i].userPassword;
+      let asciiString = atob(base64String);
+      updatedItems[i].userPassword = asciiString;
+    }
+    return updatedItems;
+  }
+
+  //Create new user
+}
